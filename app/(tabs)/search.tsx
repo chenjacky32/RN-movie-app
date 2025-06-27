ef09424ec72e7f95d5fcaece74ded5f7f8ae9be9
Jacky Chen <chenjacky32@gmail.com>
2025-06-27 12:44:40 +0700

    feat: Add functionality for search movie

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,70 @@
-import React from "react";
-import { Text, View } from "react-native";
+/* eslint-disable react-hooks/exhaustive-deps */
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import * as React from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 const Search = () => {
+  const [searchQuery, setsearchQuery] = React.useState("");
+  const { data: movies, loading, error, refetch: loadMovies, reset } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  React.useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
   return (
-    <View className="items-center justify-center flex-1">
-      <Text className="text-2xl font-bold">Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute z-0 flex-1 w-full" resizeMode="cover" />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="flex-row items-center justify-center w-full mt-20">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar placeholder="Search for a Movie..." value={searchQuery} onChangeText={(text: string) => setsearchQuery(text)} />
+            </View>
+            {loading && <ActivityIndicator size="large" color="#0000ff" className="my-3" />}
+            {error && <Text className="px-5 my-3 text-red-500">Error: {error?.message}</Text>}
+            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
+              <Text className="text-xl font-bold text-white">
+                Search Result for {""}
+                <Text className="text-accent">{searchQuery}</Text>
+              </Text>
+            )}
+          </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="px-5 mt-10">
+              <Text className="text-center text-gray-500">{searchQuery.trim() ? "No movies found" : "Search for a Movie"}</Text>
+            </View>
+          ) : null
+        }
+      />
     </View>
   );
 };
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,13 +5,15 @@ import { Image, TextInput, View } from "react-native";
 interface SearchBarProps {
   placeholder: string;
   onPress?: () => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: SearchBarProps) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: SearchBarProps) => {
   return (
     <View className="flex-row items-center px-5 py-4 rounded-full bg-dark-200">
       <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
-      <TextInput className="flex-1 ml-2 text-white" onPress={onPress} placeholder={placeholder} value="" onChangeText={() => {}} placeholderTextColor="#a8b5db" />
+      <TextInput className="flex-1 ml-2 text-white" onPress={onPress} placeholder={placeholder} value={value} onChangeText={onChangeText} placeholderTextColor="#a8b5db" />
     </View>
   );
 };
